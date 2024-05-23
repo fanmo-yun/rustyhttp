d76701f27c0b42116c28f32ac8788820e571b44a
fanmo-yun <xiaomojun9527@outlook.com>
2024-05-23 22:11:00 +0800

    write request handle

--- src/http/handle.rs
+++ src/http/handle.rs
@@ -1,7 +1,10 @@
 use std::error::Error;
-use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpStream};
+use tokio::{
+    io::{self, AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader, BufWriter},
+    net::TcpStream,
+};
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 pub struct Handler;
 
 impl Handler {
@@ -9,14 +12,53 @@ impl Handler {
         Self
     }
 
-    pub async fn process_client(self, mut client: TcpStream) -> Result<(), Box<dyn Error>> {
-        let (mut reader, mut writer) = client.split();
-        let mut buf = [0u8; 1024];
-        let n = reader.read(&mut buf).await?;
-        println!("{}", String::from_utf8_lossy(&buf[..n]));
-        let response = "HTTP/1.1 200 OK\r\nContent-Length: 11\r\n\r\nHello world";
-        writer.write_all(response.as_bytes()).await?;
+    pub async fn read_request<T: AsyncReadExt + Unpin>(
+        &self,
+        mut reader: BufReader<T>,
+    ) -> io::Result<String> {
+        let mut http_request = Vec::new();
+        let n = reader.read_until(b'\n', &mut http_request).await?;
+        let request_str = String::from_utf8_lossy(&http_request[..n])
+            .trim_end()
+            .to_string();
+        Ok(request_str)
+    }
+
+    pub async fn write_response<T: AsyncWriteExt + Unpin>(
+        &self,
+        mut writer: BufWriter<T>,
+        context: &str,
+    ) -> io::Result<()> {
+        writer.write_all(context.as_bytes()).await?;
         writer.flush().await?;
+        Ok(())
+    }
+
+    pub async fn process_client(&self, mut client: TcpStream) -> Result<(), Box<dyn Error>> {
+        let (reader, writer) = client.split();
+
+        let buf_reader = BufReader::with_capacity(1024, reader);
+        let buf_writer = BufWriter::with_capacity(1024, writer);
+
+        let req = self.read_request(buf_reader).await?;
+        self.process_request_path(req, buf_writer).await?;
+
+        Ok(())
+    }
+
+    pub async fn process_request_path<T: AsyncWriteExt + Unpin>(
+        &self,
+        request: String,
+        writer: BufWriter<T>,
+    ) -> io::Result<()> {
+        let request_parts: Vec<&str> = request.split_whitespace().collect();
+        let method = request_parts[0];
+        let path = request_parts[1];
+
+        if method == "GET" {
+            let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", path.len(), path);
+            self.write_response(writer, &response).await?;
+        }
         Ok(())
     }
 }
--- src/http/server.rs
+++ src/http/server.rs
@@ -4,6 +4,7 @@ use tokio::net::TcpListener;
 
 use super::handle::Handler;
 
+#[derive(Debug)]
 pub struct Server {
     handle: Arc<Handler>
 }
@@ -17,6 +18,7 @@ impl Server {
 
     pub async fn run(self) -> Result<(), Box<dyn Error>> {
         let listener = TcpListener::bind("127.0.0.1:3000").await?;
+        println!("server running on http://127.0.0.1:3000");
         loop {
             let (stream, _addr) = listener.accept().await?;
             let handler = Arc::clone(&self.handle);
